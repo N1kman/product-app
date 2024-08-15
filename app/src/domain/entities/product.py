229476048958284032732eb8a06df10221a70568
Mikita Hil <gilnikita026@gmail.com>
2024-08-15 17:39:52 +0300

    update product entities

--- app/src/domain/entities/product.py
+++ app/src/domain/entities/product.py
@@ -16,10 +16,22 @@ class ProductManufacturer(BaseModel):
     email: EmailStr
 
 
-class Product(BaseModel):
+class ProductManufacturerRead(ProductManufacturer):
     id: int
+
+
+class Product(BaseModel):
     name: str
     description: str
     price: float
     category: ProductCategory
     manufacturer: ProductManufacturer
+
+
+class ProductRead(Product):
+    id: int
+    name: str
+    description: str
+    price: float
+    category: ProductCategory
+    manufacturer: ProductManufacturerRead
