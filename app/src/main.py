dd3ea176f306ba7c0e028d461df1e916d8b63173
Mikita Hil <gilnikita026@gmail.com>
2024-08-19 23:06:59 +0300

    add main file

---
+++ app/src/main.py
@@ -0,0 +1,25 @@
+import asyncio
+from contextlib import asynccontextmanager
+
+import uvicorn
+from fastapi import FastAPI
+
+from src.domain.entities import Product, ProductCategory
+from src.infrastructure.api.v1.product import router as product_router
+from src.infrastructure.configs import APIConfig
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    yield
+    # shutdown
+
+main_app = FastAPI(
+    lifespan=lifespan
+)
+
+main_app.include_router(product_router)
+
+if __name__ == "__main__":
+    uvicorn.run("src.main:main_app", port=APIConfig().port, host=APIConfig().host, reload=True)
