cba645f631f36e10332452a7191e11a39cc91cab
Mikita Hil <gilnikita026@gmail.com>
2024-08-29 22:57:32 +0300

    add broker interface

---
+++ app/src/domain/interfaces/broker.py
@@ -0,0 +1,9 @@
+from abc import ABC, abstractmethod
+
+from pydantic import BaseModel
+
+
+class BrokerProducer(ABC):
+    @abstractmethod
+    async def send_message(self, topic: str, obj: BaseModel):
+        pass
