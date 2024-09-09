c172c80e94be21671ea409369101a7739ce710c7
Mikita Hil <gilnikita026@gmail.com>
2024-09-09 11:29:42 +0300

    update translator

--- app/src/infrastructure/translator/mbart.py
+++ app/src/infrastructure/translator/mbart.py
@@ -5,8 +5,8 @@ import enum
 from pydantic import BaseModel
 from transformers import MBartForConditionalGeneration, MBart50TokenizerFast
 
-from src.domain.interfaces import Language, ITranslator
 from src.domain.interfaces.translator import IAsyncTranslator
+from src.domain.language import LanguageEnum
 
 model_name = "facebook/mbart-large-50-many-to-many-mmt"
 model = MBartForConditionalGeneration.from_pretrained(model_name)
@@ -15,8 +15,7 @@ tokenizer = MBart50TokenizerFast.from_pretrained(model_name)
 
 class MBartTranslator(IAsyncTranslator):
 
-    @staticmethod
-    async def async_translate_str(string: str, src_lang: Language, target_lang: Language):
+    async def async_translate_str(self, string: str, src_lang: LanguageEnum, target_lang: LanguageEnum):
         tokenizer.src_lang = src_lang.value
         encoded = await asyncio.to_thread(tokenizer, string, return_tensors="pt")
         generated_tokens = await asyncio.to_thread(model.generate, **encoded,
@@ -25,7 +24,7 @@ class MBartTranslator(IAsyncTranslator):
         res = await asyncio.to_thread(tokenizer.batch_decode, generated_tokens, skip_special_tokens=True)
         return res[0]
 
-    async def async_translate_obj(self, obj: BaseModel, src_lang: Language, target_lang: Language):
+    async def async_translate_obj(self, obj: BaseModel, src_lang: LanguageEnum, target_lang: LanguageEnum):
         translated_obj = copy.deepcopy(obj)
         tokenizer.src_lang = src_lang.value
 
