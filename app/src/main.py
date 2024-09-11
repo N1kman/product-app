c30387a0a704836fb8a14092c0eacc9c78c78fd7
Mikita Hil <gilnikita026@gmail.com>
2024-09-11 21:54:59 +0300

    update main

--- app/src/main.py
+++ app/src/main.py
@@ -1,64 +1,28 @@
-import asyncio
 from contextlib import asynccontextmanager
 
 import uvicorn
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
-from src.domain.interfaces import Language
+from src.infrastructure.api import value_error_exception_handler
 from src.infrastructure.api.utils import allow_methods, allow_headers
-from src.infrastructure.api.v1.product import router as product_router
-from src.infrastructure.api.v1.customer import router as customer_router
-from src.infrastructure.api.v1.order import router as order_router
-from src.infrastructure.configs import api_config, kafka_config
-from src.infrastructure.kafka import KafkaProducer, KafkaConsumer
-from src.infrastructure.repositories import RuDBRepository, DeDBRepository, EnDBRepository, \
-    payment_options_en, payment_options_ru, payment_options_de
+from src.infrastructure.api.v1 import router as api_v1_router
+from src.infrastructure.configs import api_config
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    await KafkaProducer.create_topic(kafka_config.KAFKA_TOPIC)
-    tasks = list()
-    tasks.append(
-        asyncio.create_task(
-            KafkaConsumer.consume_messages(
-                kafka_config.KAFKA_GROUP_ID_RU,
-                RuDBRepository(),
-                Language.en,
-                Language.ru
-            )
-        )
-    )
-    tasks.append(
-        asyncio.create_task(
-            KafkaConsumer.consume_messages(
-                kafka_config.KAFKA_GROUP_ID_DE,
-                DeDBRepository(),
-                Language.en,
-                Language.de
-            )
-        )
-    )
-    async with EnDBRepository() as session_en:
-        async with RuDBRepository() as session_ru:
-            async with DeDBRepository() as session_de:
-                await session_en.init_db(payment_options_en)
-                await session_ru.init_db(payment_options_ru)
-                await session_de.init_db(payment_options_de)
     yield
-    for task in tasks:
-        if not task.done():
-            task.cancel()
 
 
 main_app = FastAPI(
     lifespan=lifespan
 )
 
-main_app.include_router(product_router)
-main_app.include_router(customer_router)
-main_app.include_router(order_router)
+main_app.include_router(api_v1_router)
+
+main_app.add_exception_handler(ValueError, value_error_exception_handler)
+
 
 main_app.add_middleware(
     CORSMiddleware,
